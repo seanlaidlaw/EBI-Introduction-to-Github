448bd2c20ec126989539b90bf1b383b5031bd1a7
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2021-02-15 10:57:23 +0100

    add Chiara's bwa shell commands

---
+++ bwa_alignment_sort_index_merge.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for n in {1..2}
+do 
+	bwa mem -R '@RG\tID:'"$n"'\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
+	Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa \
+	lane$n/s-7-1.trim.paired.fastq lane$n/s-7-2.trim.paired.fastq | \
+	samtools view -b - | \
+	samtools sort - -o lane${n}_sorted.bam  && 
+	
+	samtools index lane${n}_sorted.bam
+done
+
+picard MergeSamFiles INPUT=lane1_sorted.bam INPUT=lane2_sorted.bam OUTPUT=library.bam
+
+samtools index library.bam
