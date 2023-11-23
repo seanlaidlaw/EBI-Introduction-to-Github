6226ef2e78c2cfb3f1544ffae1a362dc74b8eb79
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2023-11-23 14:23:45 +0000

    add ".sh" suffix to picard to enable running with updated VMs where picard is wrappered

--- bwa_alignment_sort_index_merge.sh
+++ bwa_alignment_sort_index_merge.sh
@@ -3,18 +3,15 @@
 # something seems to be missing here that means this code doesn't run
 # an error occurs saying 'Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa' has no index
 
-
-for n in {1..2}
-do
+for n in {1..2}; do
 	bwa mem -R '@RG\tID:'"$n"'\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
-	Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa \
-	lane$n/s-7-1.trim.paired.fastq lane$n/s-7-2.trim.paired.fastq | \
-	samtools view -b - | \
-	samtools sort - -o lane${n}_sorted.bam  &&
-
-	samtools index lane${n}_sorted.bam
+		Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa \
+		lane$n/s-7-1.trim.paired.fastq lane$n/s-7-2.trim.paired.fastq |
+		samtools view -b - |
+		samtools sort - -o lane${n}_sorted.bam &&
+		samtools index lane${n}_sorted.bam
 done
 
-picard MergeSamFiles INPUT=lane1_sorted.bam INPUT=lane2_sorted.bam OUTPUT=library.bam
+picard.sh MergeSamFiles INPUT=lane1_sorted.bam INPUT=lane2_sorted.bam OUTPUT=library.bam
 
 samtools index library.bam
